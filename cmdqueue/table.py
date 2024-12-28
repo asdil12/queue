ca8d864e5146263c45039393fa725ad1dfe45111
Dominik Heidler <dheidler@suse.de>
2024-12-28 17:51:30 +0100

    Enable multi column limiting

--- cmdqueue/table.py
+++ cmdqueue/table.py
@@ -1,19 +1,54 @@
 import os
 from termcolor import colored
 
-def print_row(col_lens, row, separator='|', space=' ', outer_space=True, limit_col=None, separator_on_extra_rows=True, colors=None):
-    line_length = sum(col_lens) + 3*(len(col_lens)-1) + (2 if outer_space else 0)
-    if limit_col != None:
+def print_row(col_lens, row, separator='|', space=' ', outer_space=True, limit_cols=None, separator_on_extra_rows=True, colors=None):
+    """
+        col_lens: list containing max size for each column
+        row: list containing cells
+        separator: separator between columns
+        space: space character to fillup empty space in each column
+        outer_space: add a space (see above) char at start & end of row
+        limit_cols: id or list of ids of column(s) to shrink to fit the table into the terminal
+        separator_on_extra_rows: continue column separators on multiline rows (will make copying text harder but looks better)
+        colors: list of same length as row containing color names for the columns (each entry can be None for default color)
+    """
+
+    limit_col_max_len = max(col_lens) # this value is never used but python complains otherwise
+    limit_col = None
+    col_lens = col_lens.copy()
+    if limit_cols != None:
+        # try to limit each column mentioned in limit_cols
         try:
             termwidth = os.get_terminal_size().columns
-            # without this min() the table uses the full term width if limit_col is set
-            extra_line_space = min(termwidth - line_length, 0)
-            limit_col_max_len = col_lens[limit_col] + extra_line_space
         except:
-            limit_col_max_len = col_lens[limit_col]
-        limit_col_max_len = max(limit_col_max_len, 1)
-    else:
-        limit_col_max_len = max(col_lens)
+            termwidth = None
+        if isinstance(limit_cols, int):
+            limit_cols = [limit_cols]
+        else:
+            limit_cols = list(limit_cols)
+        for i, col in enumerate(limit_cols):
+            if termwidth:
+                # without this min() the table uses the full term width if limit_col is set
+                line_length = sum(col_lens) + 3*(len(col_lens)-1) + (2 if outer_space else 0)
+                extra_line_space = min(termwidth - line_length, 0)
+                limit_col_max_len = col_lens[col] + extra_line_space
+            else:
+                limit_col_max_len = col_lens[col]
+            if limit_col_max_len > 1:
+                limit_col = col
+                break
+            else:
+                # we would only have enough space to display … so let's drop the column completely
+                # and try to limit the next one in the list
+                row.pop(col)
+                col_lens.pop(col)
+                if colors:
+                    colors.pop(col)
+                # recalculate col ids in limit_cols
+                for j in range(i, len(limit_cols)):
+                    if limit_cols[j] > col:
+                        limit_cols[j] -= 1
+
     extra_rows = [row]
     for in_extra_row, row in enumerate(extra_rows):
         row_normalized = []
@@ -27,15 +62,22 @@ def print_row(col_lens, row, separator='|', space=' ', outer_space=True, limit_c
                     extra_rows.append(extra_row)
                     #print(extra_rows)
                 col = col[0]
-            if limit_col == i and len(col) > limit_col_max_len:
-                col = col[:limit_col_max_len-1] + '…'
             if limit_col == i:
+                if len(col) > limit_col_max_len:
+                    if limit_col_max_len == 2:
+                        col = col[:limit_col_max_len-1] + '…'
+                    else:
+                        # remove chars in the middle to hopefully preserve important stuff
+                        col = col[:limit_col_max_len-1] + '…'
+                # fill up with spaces to ensure that columns of each row stay below each other
                 row_normalized.append(col.ljust(limit_col_max_len, space))
             else:
+                # fill up with spaces to ensure that columns of each row stay below each other
                 row_normalized.append(col.ljust(col_lens[i], space))
         if colors:
             for i, color in enumerate(colors):
-                row_normalized[i] = colored(row_normalized[i], color)
+                if color:
+                    row_normalized[i] = colored(row_normalized[i], color)
         if in_extra_row and not separator_on_extra_rows:
             line = (space * 3).join(row_normalized)
         else:
@@ -63,13 +105,14 @@ def print_table(headers, rows, separator='|', outer_space=True, limit_col=None, 
         separator_on_extra_rows: support multiline fields (field being a list instead of a string)
         colors: two dimensional array containing table colors
     """
+    # generate list containing max size for each column
     col_lens = [get_col_len(hc) for hc in headers or rows[0]]
     for row in rows:
         for i, col_len in enumerate(col_lens):
             col_lens[i] = max(col_len, get_col_len(row[i]))
     if headers:
-        print_row(col_lens, headers, separator, outer_space=outer_space, limit_col=limit_col)
-        print_row(col_lens, ['']*len(col_lens), '+', '-', outer_space, limit_col=limit_col)
+        print_row(col_lens, headers, separator, outer_space=outer_space, limit_cols=limit_col)
+        print_row(col_lens, ['']*len(col_lens), '+', '-', outer_space, limit_cols=limit_col)
     for i, row in enumerate(rows):
         row_colors = colors[i] if colors else None
-        print_row(col_lens, row, separator, outer_space=outer_space, limit_col=limit_col, separator_on_extra_rows=separator_on_extra_rows, colors=row_colors)
+        print_row(col_lens, row, separator, outer_space=outer_space, limit_cols=limit_col, separator_on_extra_rows=separator_on_extra_rows, colors=row_colors)
